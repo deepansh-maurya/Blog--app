d94b7e312713d952871dffe4266fd63be3d1ac57
deepansh <depanshurya@gamil.com>
2024-02-24 18:30:39 +0530

    routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Myfeed from "./pages/myfeed/index.js";
+import Login from "./pages/login/index.js";
+import Signup from "./pages/signup/index.js";
+import Write from "./pages/write/index.js";
+import Profile from "./pages/profile/index.js";
+import Explore from "./pages/explore/index.js";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/myfeed" element={<Myfeed />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/write" element={<Write />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/explore" element={<Explore />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
