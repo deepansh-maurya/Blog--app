f60e976ca1e2fc2c3061c8bad2e2a34b7a8d65e3
deepansh <mauryadeepansh40@gmail.com>
2024-03-04 17:52:36 +0530

    search done

--- src/components/Getresults.jsx
+++ src/components/Getresults.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useBlog } from "../global/blogcontext";
 import { databases } from "../appwriteBackend/database/databse";
 import conf from "../conf/conf";
@@ -6,15 +6,27 @@ import { Query } from "appwrite";
 
 export default function Getresults() {
   const { search, setsearch } = useBlog();
-
+  const [blogs, setblogs] = useState([]);
+  console.log(blogs);
   async function getSearchResults() {
     try {
-      const promise = await databases.listDocuments(
+      // const promise = await databases.listDocuments(
+      //   conf.databse_id,
+      //   conf.article_id,
+      //   [Query.orderAsc("tags")]
+      // );
+      // const documents = promise.documents;
+      // documents.forEach((item, index) => {
+      //   if (item.tags.includes(search)) {
+      //     setblogs((prev) => [...prev, item]);
+      //   }
+      // });
+      const promise2 = await databases.listDocuments(
         conf.databse_id,
         conf.article_id,
-        [Query.equal("category", search)]
+        [Query.equal("category", "science")]
       );
-      console.log(promise);
+      setblogs((prev) => [...prev, promise2.documents]);
     } catch (error) {
       console.log(error);
     }
